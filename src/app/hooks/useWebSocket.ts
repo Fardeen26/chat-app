f8624d01c1e4d3f2c687cf324b11a2c42bc34160
Fardeen Mansuri <fardeenmansuri0316@gmail.com>
2024-11-25 19:52:40 +0530

    feat: created hooks

---
+++ src/app/hooks/useWebSocket.ts
@@ -0,0 +1,80 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useEffect, useRef, useState, useCallback } from 'react'
+import { useRoomId } from './useRoomId'
+
+type ConnectionStatus = 'connecting' | 'connected' | 'disconnected'
+
+export interface WebSocketMessage {
+    type: string
+    payload: any
+}
+
+export function useWebSocket() {
+    const [connectionStatus, setConnectionStatus] = useState<ConnectionStatus>('connecting')
+    const [lastMessage, setLastMessage] = useState<WebSocketMessage | null>(null)
+    const ws = useRef<WebSocket | null>(null)
+    const reconnectTimeoutRef = useRef<NodeJS.Timeout | null>(null)
+    const { setCurrentRoomId } = useRoomId();
+
+    const connect = useCallback(() => {
+        console.log('Attempting to connect to WebSocket')
+        ws.current = new WebSocket('ws://localhost:8080')
+
+        ws.current.onopen = () => {
+            console.log('WebSocket connected')
+            setConnectionStatus('connected')
+            if (reconnectTimeoutRef.current) {
+                clearTimeout(reconnectTimeoutRef.current)
+                reconnectTimeoutRef.current = null
+            }
+        }
+
+        ws.current.onclose = () => {
+            console.log('WebSocket disconnected')
+            setConnectionStatus('disconnected')
+            reconnectTimeoutRef.current = setTimeout(connect, 3000)
+        }
+
+        ws.current.onerror = (error) => {
+            console.error('WebSocket error:', error)
+            setConnectionStatus('disconnected')
+        }
+
+        ws.current.onmessage = (event) => {
+            const message = JSON.parse(event.data)
+            if (message.type == 'roomCreated') {
+                setCurrentRoomId(message.payload.roomId);
+            }
+
+            setLastMessage(message)
+        }
+    }, [])
+
+    useEffect(() => {
+        connect()
+        return () => {
+            if (ws.current) {
+                console.log('Closing WebSocket connection')
+                ws.current.close()
+            }
+            if (reconnectTimeoutRef.current) {
+                clearTimeout(reconnectTimeoutRef.current)
+            }
+        }
+    }, [connect])
+
+    const sendMessage = useCallback((message: WebSocketMessage) => {
+        if (ws.current?.readyState === WebSocket.OPEN) {
+            console.log('Sending message:', message)
+            if (message.type == 'leave') {
+                setCurrentRoomId('');
+            }
+            ws.current.send(JSON.stringify(message))
+        } else {
+            console.error('WebSocket is not open. Current state:', ws.current?.readyState)
+        }
+    }, [])
+
+    return { connectionStatus, lastMessage, sendMessage }
+}
+
